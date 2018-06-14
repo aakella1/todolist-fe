c14a23d95d73ecf500e5039e12a37a993784d03b
aakella1 <akella_anand@yahoo.com>
2018-06-14 12:06:32 -0400

    Implementing the store and actions

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -10,7 +10,9 @@
       </md-list-item>
       <!-- TODO - SVG for use in Lab3 -->
        <md-button class="important-flag" @click="markImportant()">
-     <svg :class="{'red flag': todoItem.important}" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" ><path d="M0 0h24v24H0z" fill="none"/><path d="M14.4 6L14 4H5v17h2v-7h5.6l.4 2h7V6z"/></svg>
+
+     <svg :class="{'red-flag': todoItem.important}" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" ><path d="M0 0h24v24H0z" fill="none"/><path d="M14.4 6L14 4H5v17h2v-7h5.6l.4 2h7V6z"/></svg>
+
  </md-button>
     </div>
   </div>
